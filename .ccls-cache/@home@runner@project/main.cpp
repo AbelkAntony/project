fab838e46c4e1f55371a2186da62ed38952442e6
ABEL K ANTONY <abelkantony98@gmail.com>
2023-02-27 15:18:37 +0000

    update

--- .ccls-cache/@home@runner@project/main.cpp
+++ .ccls-cache/@home@runner@project/main.cpp
@@ -30,26 +30,93 @@ public:
 class SafeHouse
 {
 private:
+	//variables
+	float time = 10.00;
 	int option;
 	int n = 0;
-	Character *list[7] ;
-	void DisplayAvailableCharacters()
+	Character *playerList[7] ;
+	//function
+	int Rand(int lowerLimit, int upperLimit)
+	{
+		int randomNumber = lowerLimit + (rand()%(upperLimit - lowerLimit + 1));
+		return randomNumber;
+	}
+	void DisplayAllCharacters()
 	{
 		for(int i=0; i<n; i++)
-			list[n]->Display();
+		{
+			cout<<endl<<endl;
+			cout<<"\nCHARACTER "<<i+1;
+			playerList[i]->Display();
+		}
+	}
+
+	void SwapCharacter()
+	{
+		bool swap = true;
+		while(swap)
+		{
+			int changeThis;
+			int chanageThat;
+			Character *ptr;
+			DisplayAllCharacters();
+			cout<<"\nSELECT THE NUMBER OF CHARCTER TO CHANGED : ";
+			cin>>changeThis;
+			cout<<"\nSELECT THE NUMBER OF CHARACTER TO REPLACE : ";
+			cin>>chanageThat;
+			ptr = playerList[changeThis-1];
+			playerList[changeThis-1]=playerList[chanageThat-1];
+			playerList[chanageThat-1]=ptr;
+			cout<<"DO YOU WANT TO SWAP AGAIN?\n1. YES\n2. NO";
+			cin>>option;
+			if(option==2)
+				swap = false;
+		}
+	}
+
+	//function to create enemy
+	void CreateEnemy()
+	{
+		
+	}
+	//function to fight
+	void Fight()
+	{
+		CreateEnemy();	
+	}
+
+	//function to explore
+	void Explore()
+	{
+		option = Rand(1,2);
+		switch(option)
+		{
+			case 1:
+			Fight();
+			break;
+			case 2:
+			//Chest();
+			break;
+		}
+	}
+
+	//function to shop
+	void Shop()
+	{
+			
 	}
 public:
 	SafeHouse()
 	{
-		list[0] = new Character("LADY WAGON",100,84,52);
+		playerList[0] = new Character("LADY WAGON",100,84,52);
 		n=1;
 	}
 	void Play()
 	{
-		while (list[0] != NULL)
+		while (playerList[0] != NULL)
 		{
-			cout<<"\nAVAILABLE CHARACTERS = ";
-			DisplayAvailableCharacters();
+			cout<<"\nAVAILABLE CHARACTERS ";
+			DisplayAllCharacters();
 			if(n>3)
 			{
 				cout<<"\nDO YOU WANT TO SWAP CHARCTERS\n1. YES\n2. NO";
@@ -68,8 +135,25 @@ public:
 			}
 			cout<<"\n1. EXPLORE THE WILD";
 			cout<<"\n2. GO TO SHOP";
+			cout<<"\n3. EXIT GAME";
 			cout<<"\nENTER YOUR OPTION : ";
 			cin>>option;
+			switch(option)
+			{
+				case 1:
+				if(time>=10.00)
+					Explore();
+				else
+					cout<<"\nTIME IS "<<time<<"YOU CANNOT EXPLORE AT THIS TIME";
+				break;
+				case 2:
+				Shop();
+				break;
+				case 3:
+				return;
+				default:
+				cout<<"\nINVALID OPTION";
+			}
 		}
 	}
 };
@@ -87,7 +171,7 @@ private:
 	//function to display game story
 	void DisplayGameStory()
 	{
-		cout<<"\nYOU ARE IN A NEW WORLD NAMED WAKANDA. YOU NEED TO TAKE OVER THIS WORLD BY CONQUER THE ANIMALS AND ENEMIES IN THE WORLD . YOU CAN EXPLORE THE WORLD BETWEEN 10.00HR TO 20.00 HRS. YOU HAVE A HOUSE IN THIS WORLD INITIALLY YOU HAVE ONE CHARACTER AVAILABLE IN THE HOUSE LATER ON YOU CAN UNLOCK MORE CHARACTERS BY EXPLORE THEM. AFTER EACH MATCH YOU CAN COME BACK AND CHANGE THE CHARACTER. YOU CAN HAVE UPTO 3 CHARATERS AT THE TIME OF EXPLORE EACH CHARACTER HAVE DIFFERENT ABILITIES AND EACH ABILITIES TAKE DIFFERENT TURN CYCLE \n\nALL THE BEST";
+		cout<<"\nYOU ARE IN A NEW WORLD NAMED WAKANDA. YOU NEED TO TAKE OVER THIS WORLD BY CONQUER THE ANIMALS AND ENEMIES IN THE WORLD . YOU CAN EXPLORE THE WORLD BETWEEN 10.00HR TO 20.00 HRS. YOU HAVE A HOUSE IN THIS WORLD INITIALLY YOU HAVE ONE CHARACTER AVAILABLE IN THE HOUSE LATER ON YOU CAN UNLOCK MORE CHARACTERS BY EXPLORE THEM. AFTER EACH MATCH YOU CAN COME BACK AND CHANGE THE CHARACTER. YOU CAN HAVE UPTO 3 CHARATERS AT THE TIME OF EXPLORE EACH CHARACTER HAVE DIFFERENT ABILITIES AND EACH ABILITIES TAKE DIFFERENT TURN CYCLE . YOU CAN HAVE MANY CHARACTERS BUT WHEN YOU GO TO EXPLORE YOU CAN HAVE ONLY FIRST THREE CHARACTER \n\nALL THE BEST";
 	}
 	
 public:
@@ -106,6 +190,7 @@ public:
 
 int main()
 {
+	srand(time(0));
 	IntoTheWild newGame;
 	newGame.Game();
 	return 0;
